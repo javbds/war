76c445a28dd4d1b62cc20dff403a532563326462
Javier Guerra <javchewie@gmail.com>
2026-09-10 16:02:08 -0500

    Restore original War game

--- war.py
+++ war.py
@@ -3,10 +3,12 @@ from random import shuffle
 class Card:
     suits=("spades", "hearts", "diamonds", "clubs")
 
-    values=(None, None, "2", "3", "4',
-            "5", "6", "7", "8", "9",
+    values = (
+        None, None, "2", "3", "4",
+        "5", "6", "7", "8", "9",
 	    "10", "Jack", "Queen",
-	    "King', "Ace")
+	    "King", "Ace"
+    )
 
     def __init__(self, v, s):
         """suit + value are ints"""
@@ -23,7 +25,7 @@ class Card:
                 return False
         return False
      
-     def __gt__(self, c2):
+    def __gt__(self, c2):
          if self.value>c2.value:
              return True
          if self.value==c2.value:
@@ -33,11 +35,10 @@ class Card:
                  return False
          return False
 
-      def __repr__(self):
-          v= self.value[self.value] +\
-             "of" +\
-             self.suits[self.suit]
-          return v
+    def __repr__(self):
+        value = self.values[self.value]
+        suit = self.suits[self.suit]
+        return f"{value} of {suit}"
 
 class Deck:
     def  __init__(self):
